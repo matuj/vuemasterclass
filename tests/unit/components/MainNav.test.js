50be9ac0611cb7c7edbd9d74df9b63860b136c6d
matu.kimaru <matu.kimaru@gmail.com>
2022-10-16 12:44:56 +0300

    Section 8

--- tests/unit/components/MainNav.test.js
+++ tests/unit/components/MainNav.test.js
@@ -6,4 +6,18 @@ describe("MainNav", () => {
     const wrapper = mount(MainNav);
     expect(wrapper.text()).toMatch("DayDream Careers");
   });
+
+  it("Displays menu items for navigation", () => {
+    const wrapper = mount(MainNav);
+    const navMenuItems = wrapper.findAll("[data-test='main-nav-list-item']");
+    const navMenuTexts = navMenuItems.map((navMenuItem) => navMenuItem.text());
+    expect(navMenuTexts).toEqual([
+      "Teams",
+      "Locations",
+      "Life at DayDream",
+      "How we hire",
+      "Students",
+      "Jobs",
+    ]);
+  });
 });
