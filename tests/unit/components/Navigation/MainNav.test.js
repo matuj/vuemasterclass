5d11cd2faeca170f5d543863d53fd3f2a8e53889
matu.kimaru <matu.kimaru@gmail.com>
2022-10-23 17:05:09 +0300

    Section 15

--- tests/unit/components/Navigation/MainNav.test.js
+++ tests/unit/components/Navigation/MainNav.test.js
@@ -1,14 +1,22 @@
-import { shallowMount } from "@vue/test-utils";
+import { shallowMount, RouterLinkStub } from "@vue/test-utils";
 import MainNav from "@/components/Navigation/MainNav.vue";
 
 describe("MainNav", () => {
+  const createConfig = () => ({
+    global: {
+      stubs: {
+        "router-link": RouterLinkStub,
+      },
+    },
+  });
+
   it("Displays company name", () => {
-    const wrapper = shallowMount(MainNav);
+    const wrapper = shallowMount(MainNav, createConfig());
     expect(wrapper.text()).toMatch("Dream Careers");
   });
 
   it("Displays menu items for navigation", () => {
-    const wrapper = shallowMount(MainNav);
+    const wrapper = shallowMount(MainNav, createConfig());
     const navMenuItems = wrapper.findAll("[data-test='main-nav-list-item']");
     const navMenuTexts = navMenuItems.map((navMenuItem) => navMenuItem.text());
     expect(navMenuTexts).toEqual([
@@ -23,7 +31,7 @@ describe("MainNav", () => {
 
   describe("When user is logged out", () => {
     it("Prompts user to sign in", () => {
-      const wrapper = shallowMount(MainNav);
+      const wrapper = shallowMount(MainNav, createConfig());
       const loginButton = wrapper.find("[data-test='login-button']");
       expect(loginButton.exists()).toBe(true);
     });
@@ -31,18 +39,20 @@ describe("MainNav", () => {
 
   describe("When user is logged in", () => {
     it("Displays user profile picture", async () => {
-      const wrapper = shallowMount(MainNav);
+      const wrapper = shallowMount(MainNav, createConfig());
       let profileImage = wrapper.find("[data-test='profile-image']");
       expect(profileImage.exists()).toBe(false);
+
       const loginButton = wrapper.find("[data-test='login-button']");
       await loginButton.trigger("click");
       profileImage = wrapper.find("[data-test='profile-image']");
       expect(profileImage.exists()).toBe(true);
     });
     it("Displays subnavigation menu with additional information", async () => {
-      const wrapper = shallowMount(MainNav);
+      const wrapper = shallowMount(MainNav, createConfig());
       let subnav = wrapper.find("[data-test='subnav']");
       expect(subnav.exists()).toBe(false);
+
       const loginButton = wrapper.find("[data-test='login-button']");
       await loginButton.trigger("click");
       subnav = wrapper.find("[data-test='subnav']");
--- tests/unit/components/Shared/TextInput.test.js
+++ tests/unit/components/Shared/TextInput.test.js
@@ -4,7 +4,9 @@ import TextInput from "@/components/Shared/TextInput.vue";
 describe("TextInput", () => {
   it("Communicates that user has entered character", () => {
     const wrapper = mount(TextInput, {
-      modelValue: "",
+      props: {
+        modelValue: "",
+      },
     });
     const input = wrapper.find("input");
     input.setValue("N");
