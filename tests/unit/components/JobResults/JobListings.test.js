32a2f1e3b58c5c410f6c5b9a8caeae4a2787f0d6
matu.kimaru <matu.kimaru@gmail.com>
2022-11-05 10:33:25 +0300

    Section 18

--- tests/unit/components/JobResults/JobListings.test.js
+++ tests/unit/components/JobResults/JobListings.test.js
@@ -1,3 +1,21 @@
+import { shallowMount, flushPromises } from "@vue/test-utils";
 import axios from "axios";
+jest.mock("axios");
 
-console.log(axios);
+import JobListings from "@/components/JobResults/JobListings.vue";
+
+describe("JobListings", () => {
+  it("Fetches jobs", () => {
+    axios.get.mockResolvedValue({ data: [] });
+    shallowMount(JobListings);
+    expect(axios.get).toHaveBeenCalledWith("http://localhost:3000/jobs");
+  });
+
+  it("Creates a job listing for each resolved job", async () => {
+    axios.get.mockResolvedValue({ data: Array(15).fill({}) });
+    const wrapper = shallowMount(JobListings);
+    await flushPromises();
+    const jobListings = wrapper.findAll("[data-test='job-listing']");
+    expect(jobListings).toHaveLength(15);
+  });
+});
