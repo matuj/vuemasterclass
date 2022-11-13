acbdad972282b58a8e64a69a3735df39fec88874
matu.kimaru <matu.kimaru@gmail.com>
2022-11-13 19:42:08 +0300

    Section 19

--- src/playground.js
+++ src/playground.js
@@ -1,11 +1,3 @@
-const axios = require("axios");
-//import axios from "axios";
+const sushi = ["tuna", "salmon", "eel", "shrimp", "octopus", "uni"];
 
-const url = "http://localhost:3000/jobs";
-
-const fetchv2 = async () => {
-  const response = await axios.get(url);
-  console.log(response.data);
-};
-
-fetchv2();
+console.log(sushi.slice(2, 4));
